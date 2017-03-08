84f2756ec38befc16c9d8184a5e0502336f4122c
XvonabuR <webdev.xvonabur@gmail.com>
2017-03-08 23:10:25 +0300

    Task 4. Prepare backend

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+module Api
+  class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :update, :destroy]
+
+    def index
+      @posts = Post.all
+
+      render json: @posts
+    end
+
+    def show
+      render json: @post
+    end
+
+    def create
+      @post = Post.new(post_params)
+
+      if @post.save
+        render json: @post, status: :created
+      else
+        render json: @post.errors, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @post.update(post_params)
+        render json: @post
+      else
+        render json: @post.errors, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @post.destroy
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :body, :username)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  end
+end
